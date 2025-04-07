80e4cddd6445d1ae14331f9376854a69a29279a4
nadyalife2 <nadyalife1>
2025-04-07 19:35:52 +0400

    обнова

--- main3.py
+++ main3.py
@@ -4,6 +4,6 @@ nested = os.path.exists(file1)
 if nested != 0:
     print("text.txt",end= " ")
     print(os.path.dirname(file1),end= " ")
-    print("last access time", os.path.getatime(file1))
+    print("last access time", os.path.getatime(file1),"sec")
 else:
     print("Файл не существует")
