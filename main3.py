a617237f549366f48e02415ba7086441a67039b3
nadyalife2 <nadyalife1>
2025-04-07 19:14:27 +0400

    text.txt

--- main3.py
+++ main3.py
@@ -1,8 +1,8 @@
 import os
-file1=r"nested1\test.txt"
+file1=r"nested1\text.txt"
 nested = os.path.exists(file1)
 if nested != 0:
-    print("test.txt",end= " ")
+    print("text.txt",end= " ")
     print(os.path.dirname(file1),end= " ")
     print("last access time", os.path.getatime(file1))
 else:
