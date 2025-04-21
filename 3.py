a4707d7d1d6124d1b453981d0a0a9aa23ba9eca0
nadyalife2 <nadyalife1>
2025-04-21 22:20:39 +0400

    площади

--- 3.py
+++ 3.py
@@ -1,12 +1,39 @@
-def negative_number(n):
-    if len(n)==0:
-        return 0
-    count=0
-    if n[0]<0:
-        count += 1
-    return count + negative_number(n[1:])
+from math import sqrt
+
+
+class Area:
+    __count=0
+
+    @staticmethod
+    def triangle_area(a,b,c):
+        p=(a+b+c)/2
+        Area.__count+=1
+        return sqrt(p*(p-a)*(p-b)*(p-c))
+
+    @staticmethod
+    def triangle_area_2(a,h):
+        Area.__count += 1
+        return 0.5*a*h
+
+    @staticmethod
+    def square_area(a):
+        Area.__count += 1
+        return a**2
+
+    @staticmethod
+    def rectangle_area(a, b):
+        Area.__count += 1
+        return a * b
+
+    @staticmethod
+    def get_count():
+        return Area.__count
 
 
 
-lst=[-2,3,8,-11,-4,6]
-print(negative_number(lst))
+
+print("Площадь треугольника по формуле Герона", Area.triangle_area(3,4,5))
+print("Площадь треугольника через основание и высоту (6,7)", Area.triangle_area_2(6, 7))
+print("Площадь квадрата (7): ", Area.square_area(7))
+print("Площадь прямоугольника (2, 6): ", Area.rectangle_area(2,6))
+print("Количество подсчетов площади: ", Area.get_count())
