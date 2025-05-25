07a8620480392aae4a30542b0142e689af71a31d
nadyalife2 <nadyalife1>
2025-05-25 22:54:54 +0400

    csv

--- 3.py
+++ 3.py
@@ -1,24 +1,7 @@
-class Student:
-    def __init__(self, name):
-        self.name = name
-        self.note = self.Notebook()
+import csv
 
-    def show(self):
-        print(self.name, end="")
-        self.note.show()
+with open("data2.csv", "r") as f:
+    reader = csv.reader(f, delimiter=';')
+    for row in reader:
+        print(row)
 
-    class Notebook:
-        def __init__(self):
-            self.brand = "HP"
-            self.cpu = 'i7'
-            self.ram = 16
-
-        def show(self):
-            print(f" => {self.brand}, {self.cpu}, {self.ram}")
-
-
-s1 = Student("Roman")
-s2 = Student("Vladimir")
-
-s1.show()
-s2.show()
