14c09324364c3095ca59865555a48c51db4c8ee0
nadyalife2 <nadyalife4870@gmail.com>
2025-07-07 20:57:18 +0400

    Add files via upload

--- twwo/sitedb.py
+++ twwo/sitedb.py
@@ -1,6 +1,6 @@
 import sqlite3
 import os
-from flask import Flask, render_template, flash, request, g
+from flask import Flask, render_template, flash, request, g, abort
 
 from twwo.fdatabase import FDataBase
 
@@ -38,15 +38,15 @@ def get_db():
 def index():
     db = get_db()
     dbase = FDataBase(db)
-    return render_template('index.html', menu=dbase.get_menu())
+    return render_template('index.html', menu=dbase.get_menu(), posts=dbase.get_posts_anonce())
 
 
-@app.route("/add_post", methods=["POST", "GET"])
-def add_post():
-    db = get_db()
-    dbase = FDataBase(db)
-
-    return render_template('add_post.html', menu=dbase.get_menu())
+# @app.route("/add_post", methods=["POST", "GET"])
+# def add_post():
+#     db = get_db()
+#     dbase = FDataBase(db)
+#
+#     return render_template('add_post.html', menu=dbase.get_menu())
 
 @app.route("/about")
 def about():
@@ -62,12 +62,47 @@ def contact():
 
     return render_template('contact.html', menu=dbase.get_menu())
 
-@app.route("/page404", methods=["POST", "GET"])
-def page404():
+# @app.route("/page404", methods=["POST", "GET"])
+# def page404():
+#     db = get_db()
+#     dbase = FDataBase(db)
+#
+#     return render_template('page404.html', menu=dbase.get_menu())
+
+@app.route("/add_post", methods=["POST", "GET"])
+def add_post():
     db = get_db()
     dbase = FDataBase(db)
 
-    return render_template('page404.html', menu=dbase.get_menu())
+    if request.method == "POST":
+        if len(request.form["name"]) > 4 and len(request.form["post"]) > 10:
+            res = dbase.add_post(request.form["name"], request.form["post"])
+            if not res:
+                flash("Ошибка добавления статьи", category="error")
+            else:
+                flash("Статья добавлена успешно", category="success")
+        else:
+            flash("Ошибка добавления статьи", category="error")
+
+    return render_template('add_post.html', menu=dbase.get_menu())
+
+
+@app.route("/post/<int:id_post>")
+def show_post(id_post):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, post = dbase.get_post(id_post)
+    if not title:
+        abort(404)
+
+    return render_template('post.html', menu=dbase.get_menu(), title=title, post=post)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template("page404.html", title="Страница не найдена", menu=dbase.get_menu())
 
 @app.teardown_appcontext
 def close_db(error):
