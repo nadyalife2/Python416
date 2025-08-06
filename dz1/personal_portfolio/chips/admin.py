2d101578d1b08f6b9c773cbe5d6efa7c3c0e3554
nadyalife2 <nadyalife4870@gmail.com>
2025-08-06 22:12:04 +0400

    Delete dz1/personal_portfolio directory

--- dz1/personal_portfolio/chips/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from .models import Chips
-
-admin.site.register(Chips)
--- dz1/personal_portfolio/chips/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Chips(models.Model):
-     title=models.CharField(max_length=120)
-     description= models.CharField(max_length=300)
-     url= models.URLField(blank=True)
-     image = models.ImageField('chips/images/')
