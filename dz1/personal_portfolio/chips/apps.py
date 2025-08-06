1bf5fc94c89e82c95af9b764f97221529777359a
nadyalife2 <nadyalife4870@gmail.com>
2025-08-06 22:13:48 +0400

    Add files via upload

---
+++ dz1/personal_portfolio/chips/apps.py
@@ -0,0 +1,11 @@
+from django.apps import AppConfig
+
+
+class ChipsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'chips'
+
+class BlogConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'blog'
+
