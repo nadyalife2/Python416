88f08f0dc29ada83f26111acb87a4eb73499f275
nadyalife2 <nadyalife4870@gmail.com>
2025-08-27 21:19:50 +0400

    Add files via upload

---
+++ dz1/personal_portfolio/todo/views.py
@@ -0,0 +1,78 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
+from .forms import TodoForm
+from .models import Todo
+
+def home(request):
+    return render(request, 'todo/base.html')
+
+def signup_user(request):
+    if request.method == "GET":
+        return render(request,
+                  'todo/signupuser.html', {'form':
+                                           UserCreationForm()})
+    else:
+        if request.POST['password1']==request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'],
+                                                password = request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html',
+                              {'form': UserCreationForm(), 'error': 'Такое имя пользователя уже существует. Задайте другое'})
+
+        else:
+            return render(request,'todo/signupuser.html',
+            {'form':UserCreationForm(),'error':'Пароли не совпадают'})
+
+
+def current_todos(request):
+    todos = Todo.objects.filter(user=request.user, date_completed__isnull=True)
+    return render(request,
+                  'todo/currenttodos.html',{'todos':todos})
+
+def logout_user(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect('home')
+
+
+def login_user(request):
+    if request.method == "GET":
+        return render(request, 'todo/loginuser.html',
+                      {'form':AuthenticationForm()})
+    else:
+        user=authenticate(request, username=request.POST['username'],
+                          password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/loginuser.html',
+                          {'form': AuthenticationForm(),'error':'Неверные данные для входа'})
+        else:
+            login(request,user)
+            return redirect('currenttodos')
+
+def create_todo(request):
+    if request.method == "GET":
+        return render(request, 'todo/createtodo.html',{"form":TodoForm()})
+    else:
+        try:
+            form= TodoForm(request.POST)
+            new_todo = form.save(commit=False)
+            new_todo.user = request.user
+            new_todo.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request,'todo/createtodo.html',
+                          {"form":TodoForm(), 'error': 'Переданы неверные данные. Попробуйте еще раз'})
+
+
+def view_todo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk = todo_pk)
+    form = TodoForm(instance=todo)
+    return render(request, 'todo/viewtodo.html',
+                  {'todo':todo, 'form': form})
