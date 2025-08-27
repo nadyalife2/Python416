c5998402b568cfc499a377fdc6b6cb61674ba8fe
nadyalife2 <nadyalife4870@gmail.com>
2025-08-27 20:22:23 +0400

    Delete dz1/personal_portfolio directory

--- dz1/personal_portfolio/chips/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Chips
-
-def index(request):
-    projects = Chips.objects.all()
-    return render(request, 'chips/index.html',{'projects':projects})
